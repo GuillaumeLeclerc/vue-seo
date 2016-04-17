5b1178d143c87c76dd1dd6445e1de69362479679
Guillaume Leclerc <guillaume.leclerc@epfl.ch>
2016-04-17 23:20:27 +0200

    update examples (not correcs yet)

--- examples/openGraph/demo.js
+++ examples/openGraph/demo.js
@@ -1,7 +1,9 @@
 // With this option set to true open graph meta tags will be added automatically
 // according to the data you already provide to vue-seo
 Vue.use(VueSEO, {
-  autoOpenGraph: true
+  policies: {
+    title: VueSEO.policies.join(' - ', true)
+  }
 });
 
 new Vue({
